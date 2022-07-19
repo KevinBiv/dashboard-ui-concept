cc5af6c2c1a9e9e07bf2822568aeba28dadf6a0b
Kevin BIVEGETE <63170211+KevinBiv@users.noreply.github.com>
2022-07-19 10:07:22 +0200

    added top section and cards

--- src/components/Main-Section/Middle-Main-Section/index.jsx
+++ src/components/Main-Section/Middle-Main-Section/index.jsx
@@ -17,20 +17,22 @@ class MiddleMainSection extends React.Component {
                     <div>
                         <div className="top-middle-title">
                             <h3>Your Courses</h3>
-                            <button>PREMIUM</button>
+                            <button className="premium-btn">PREMIUM</button>
                         </div>
                         <div>
                             <p>26 courses</p>
                         </div>
                     </div>
-                    <div>
-                        <BsBookmark />
+                    <div className="bookmark-search">
+                        <BsBookmark className="bookmark" />
                         <TbMessageCircle2 />
                     </div>
                 </div>
 
                 <div className="middle-middle-container">
                     <Card cardPicture={php} level="BEGINNERS" technology="Fun with PHP" picture={darrel} name="Darrel Steward" percentage="68%" ratio="19/26" flag={USA} />
+                    {/* <Card cardPicture={php} level="BEGINNERS" technology="Fun with PHP" picture={darrel} name="Darrel Steward" percentage="68%" ratio="19/26" flag={USA} />
+                    <Card cardPicture={php} level="BEGINNERS" technology="Fun with PHP" picture={darrel} name="Darrel Steward" percentage="68%" ratio="19/26" flag={USA} /> */}
                 </div>
 
             </div>
